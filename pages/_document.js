c7c7ca165483b1f095559faac19b608deaf02eed
Shovon <minarshovon909@gmail.com>
2023-10-18 21:02:19 +0600

    h

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document () {
   return (
     <Html lang="en">
     <Head>
+      <meta property='og:image' content='../C-Sec.png' />
     {/* Other meta tags, link tags, or styles can be added here */}
   </Head>
       <body>
